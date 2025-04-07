6679c00934677232f08c07d9f4a0381ba6943ba8
Junha <jpark284@asu.edu>
2025-04-06 18:30:32 -0700

    build

---
+++ frontend/build/precache-manifest.85e3e63e4476b1a5d05187ad30c06d1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa21670adeeb51ce8b3dcb60f769da4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7597304a6a9307fbeea2",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "7597304a6a9307fbeea2",
+    "url": "/static/js/main.f2486ff0.chunk.js"
+  },
+  {
+    "revision": "60bc7f864dddbc003152",
+    "url": "/static/js/runtime~main.71a348c3.js"
+  }
+]);
